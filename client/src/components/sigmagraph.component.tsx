db1ac1828c1f1bc5b28245760ab30e86b5a1bdd3
Lau <l.w.a.stuurman@vu.nl>
2024-02-15 16:40:38 +0100

    small start on sigmagraph component

---
+++ client/src/components/sigmagraph.component.tsx
@@ -0,0 +1,39 @@
+/**
+ * @author Laurens Stuurman <l.w.a.stuurman@uva.nl>
+ *
+ * Uses react-force-graph to visualize the network
+ */
+
+
+import React, { useRef, useContext } from 'react';
+
+// import { Renderer } from './renderer.component'
+import { GraphDataContext } from './main.component'
+// import ForceGraph3D from 'react-force-graph-3d';
+
+import { SelectionDataContext, GlobalSettingsContext } from "./main.component"
+// import { MappingContext } from './main.component'
+// import { MappingType } from '../reducers/selectedmappings.reducer';
+// import { BasicEdge, BasicNode } from './router.component';
+import {themeContext} from './darkmode.component';
+
+export default function Graph(){
+    const { graphState } = useContext(GraphDataContext)
+
+    let nodes = []
+    graphState.nodes.data.forEach(basicNode =>{
+        nodes.push({
+            "id":basicNode.id,
+            ""
+        })
+
+    })
+    let links = []
+
+    let reactForceData = {
+        nodes:graphState.nodes,
+        links:graphState.edges
+    }
+
+    const <ForceGraph3D graphData={reactForceData}/>
+}
