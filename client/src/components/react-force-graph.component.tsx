d4682e8c0def87e393d76b774973bba15f4f4eb8
Lau <l.w.a.stuurman@vu.nl>
2024-03-19 17:28:11 +0100

    updated coords after force algorithm

--- client/src/components/react-force-graph.component.tsx
+++ client/src/components/react-force-graph.component.tsx
@@ -9,6 +9,9 @@ import React, { useRef, useContext,useEffect,useState } from 'react';
 
 // import { Renderer } from './renderer.component'
 import { GraphDataContext } from './main.component'
+import { API } from '../services/api.service'
+import { GraphDataReducerAction, GraphDataState, GraphDataReducer } from '../reducers/graphdata.reducer'
+
 import ForceGraph3D from 'react-force-graph-3d';
 import {ForceGraphMethods} from 'react-force-graph-3d';
 import ForceGraph2D from 'react-force-graph-2d';
@@ -44,7 +47,7 @@ const BUSINNESSROLE_TO_COLOR = {
   }
 
 export default function ThreeDimGraph(){
-    const { graphState } = useContext(GraphDataContext)
+    const { graphState,graphDispatch } = useContext(GraphDataContext)
     const [reactForceData, setGraphData] = useState({ nodes: [], links: [] });
 
     const fgRef = useRef();
@@ -75,7 +78,7 @@ export default function ThreeDimGraph(){
         let nodes = []
         graphState.nodes.data.forEach(basicNode =>{
             nodes.push({
-                "id":basicNode.id,
+                id:basicNode.id,
                 "name":basicNode["Business Role"],
                 "val":basicNode["Criminal Capital"],
                 "group":BUSINNESSROLE_TO_GROUP[basicNode["Business Role"]],
@@ -114,28 +117,48 @@ export default function ThreeDimGraph(){
         <ForceGraph3D 
             ref={fgRef}
             graphData={reactForceData}
+            d3AlphaDecay={.03}
+            d3AlphaMin={.002}
             nodeAutoColorBy="group"
+            onNodeDrag={()=>{
+                if (fgRef == undefined){
+                    return
+                }
+                fgRef.current.d3ReheatSimulation()}
+            }
             onNodeDragEnd={node => {
                 node.fx = node.x;
                 node.fy = node.y;
                 node.fz = node.z;
               }}
             nodeRelSize={8}
-            onEngineStop={() => {
-                console.log('ENGINE STOPPED')
-                const fgRef = useRef();
-                const fg = fgRef.current;
-
-                let nodes = fg.getGraphBbox()
-
-                console.log('got the nodes')
-                console.log(nodes)
+            onEngineTick={() => {
+                if (reactForceData.nodes[0] == undefined){
+                    return
+                }
+                console.log(reactForceData.nodes[0].x)
                 }
             }
+            onEngineStop={() => {
+                let {nodes,} = reactForceData
+                RFDataToGraphstate(nodes,graphState)
+                // graphDispatch('set')let newGraphstate = 
+                // Update graphstate in api: 
+                // API.updateGraph(graphState) 
+            }}
         />
         </div> //
 }
 
+function RFDataToGraphstate(nodes: any[],graphState:GraphDataState) {
+    // Update positions after force algorith 
+    nodes.forEach((node:any) => {
+        let graphIndex = graphState.nodes.data.findIndex((n) => n.id == node.id);
+        graphState.nodes.data[graphIndex]['position'] = {x:node['x'], y:node['y'],z:node['z']}
+    })
+
+    return graphState
+}
 
 
 
